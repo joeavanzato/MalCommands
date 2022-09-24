ca46f6c2682b8f41e4df10760bd409e71e785b9c
panscan <joeavanzato@gmail.com>
2022-09-24 14:00:01 -0400

    Tweaking loading to include array of techniques and tactics directly accessible without dynamic keying.

--- command_loader.py
+++ command_loader.py
@@ -3,6 +3,10 @@ import pymongo
 import os
 
 def import_to_mongo():
+    valid_ckc = ["Reconnaissance", "Weaponization","Delivery","Exploitation","Installation","Command and Control","Actions on Objectives"]
+    valid_risk = ["Low","Medium","High","Critical"]
+    valid_fidelity = ["Low","Medium","High"]
+    valid_os = ["Windows","Linux","Mac"]
     files = []
     for root, dir, f in os.walk("command_data"):
         for i in f:
@@ -12,8 +16,7 @@ def import_to_mongo():
     db = client.malcommands_dev
     command_collection = db.command_collection
     technique_collection = db.technique_collection
-    with open('technique_reference.yml', 'r') as f:
-        technique_data = yaml.safe_load(f)
+
     for f in files:
         print(f"Loading: {f}")
         with open(f, 'r') as f:
@@ -21,12 +24,19 @@ def import_to_mongo():
         if data is not None:
             for i in data:
                 techs = []
+                techniques = []
+                tactics = []
                 for tactic in i['mitre']:
+                    tactics.append(tactic)
                     for technique in i['mitre'][tactic]:
+                        techniques.append(technique)
                         tech_req = technique_collection.find_one({'technique_id' : technique})
                         techs.append(tech_req)
-
+                tactics = list(dict.fromkeys(tactics))
+                techniques = list(dict.fromkeys(techniques))
                 i['mitre_long'] = techs
+                i['mitre_tactics'] = tactics
+                i['mitre_techniques'] = techniques
                 if db.command_collection.count_documents({"command":i['command']}) > 0:
                     filter = {"command":f"{i['command']}"}
                     record = command_collection.update_one(filter, {"$set":i})
