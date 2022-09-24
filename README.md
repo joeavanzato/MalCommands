f6424edf431a53d87c33b9d8e4df461f297c3259
panscan <joeavanzato@gmail.com>
2022-09-23 21:39:10 -0400

    Minor Updates

--- README.md
+++ README.md
@@ -27,5 +27,28 @@ Designed to document and present in an easily searchable and filter-capable mann
 * Actions on Objectives
 
 
+### TO DOCUMENT STILL
+* AT
+* PsExec
+* RDP
+* SCShell
+* AppInstaller
+* Aspnet_Compiler
+* Atbroker
+* Bitsadmin
+* CertOC
+* CertReq
+* cmdkey
+* cmd132
+* cmstp
+* configsecuritypolicy
+* etc
+
+
+
 ### Project References
 * https://github.com/3CORESec/MAL-CL
+* https://gist.github.com/MHaggis/df2de612615ef6bd01ea936cc8351ab2
+* https://lolbas-project.github.io/
+* https://blogs.jpcert.or.jp/en/2016/01/windows-commands-abused-by-attackers.html
+* https://kwcsec.gitbook.io/the-red-team-handbook/
--- command_loader.py
+++ command_loader.py
@@ -11,12 +11,22 @@ def import_to_mongo():
     client = pymongo.MongoClient()
     db = client.malcommands_dev
     command_collection = db.command_collection
+    technique_collection = db.technique_collection
+    with open('technique_reference.yml', 'r') as f:
+        technique_data = yaml.safe_load(f)
     for f in files:
         print(f"Loading: {f}")
         with open(f, 'r') as f:
             data = yaml.safe_load(f)
         if data is not None:
             for i in data:
+                techs = []
+                for tactic in i['mitre']:
+                    for technique in i['mitre'][tactic]:
+                        tech_req = technique_collection.find_one({'technique_id' : technique})
+                        techs.append(tech_req)
+
+                i['mitre_long'] = techs
                 if db.command_collection.count_documents({"command":i['command']}) > 0:
                     filter = {"command":f"{i['command']}"}
                     record = command_collection.update_one(filter, {"$set":i})
