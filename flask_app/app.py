e864000468a3e66cbf8605c2711cdab2ce39b75e
panscan <joeavanzato@gmail.com>
2022-09-26 13:05:13 -0400

    General tweaks and 404, History PushState handling, re-writing URL and perma-linking individual commands for sharing.

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 # https://paletton.com/#uid=53w0u0kllllxE6Pv4btbBvda7G-
 
 app = Flask(__name__, template_folder='templates')
+#app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='images/favicon.ico'))
 client = MongoClient()
 db = client.malcommands_dev
 command_collection = db.command_collection
@@ -28,7 +29,19 @@ def commands(obj_id):
     except:
         abort(404)
     command = command_collection.find({'_id': object}, {})
-    return render_template('command_pop.html', command=list(command))
+    if command_collection.count_documents({'_id': object}) == 0:
+        abort(404)
+    return render_template('command_pop.html', command=list(command), baseid=obj_id)
+@app.route('/links/<obj_id>', methods=['GET'])
+def links(obj_id):
+    try:
+        object = ObjectId(obj_id)
+    except:
+        abort(404)
+    command = command_collection.find({'_id': object}, {})
+    if command_collection.count_documents({'_id': object}) == 0:
+        abort(404)
+    return render_template('command_perma.html', command=list(command))
 @app.route('/faq', methods=['GET'])
 def faq():
     return render_template('faq.html')
@@ -36,3 +49,6 @@ def faq():
 def contribute():
     return render_template('contribute.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
